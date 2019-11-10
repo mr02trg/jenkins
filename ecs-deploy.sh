0aedeabec33524575ad32af3a86addb64083da47
mr02trg <40255136+mr02trg@users.noreply.github.com>
2019-11-11 10:27:00 +1100

    Create ecs-deploy.sh

---
+++ ecs-deploy.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+TASK_FAMILY= "test-definition"
+SERVICE_NAME= "test-service"
+NEW_DOCKER_IMAGE= "626379456089.dkr.ecr.ap-southeast-2.amazonaws.com/test:${BUILD_NUMBER}"
+CLUSTER_NAME= "test-cluster"
+OLD_TASK_DEF=$(aws ecs describe-task-definition --task-definition $TASK_FAMILY --output json)
+NEW_TASK_DEF=$(echo $OLD_TASK_DEF | jq --arg NDI $NEW_DOCKER_IMAGE '.taskDefinition.containerDefinitions[0].image=$NDI')
+FINAL_TASK=$(echo $NEW_TASK_DEF | jq '.taskDefinition|{family: .family, volumes: .volumes, containerDefinitions: .containerDefinitions}')
+aws ecs register-task-definition --family $TASK_FAMILY --cli-input-json "$(echo $FINAL_TASK)"
+aws ecs update-service --service $SERVICE_NAME --task-definition $TASK_FAMILY --cluster $CLUSTER_NAME
